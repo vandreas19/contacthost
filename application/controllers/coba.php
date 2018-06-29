fac1ce4765f843bcc8fce74a43d41bf2d1b5aad7
Vicky <vandreas19@yahoo.com>
2018-06-29 17:28:10 +0700

    w

--- application/controllers/coba.php
+++ application/controllers/coba.php
@@ -1,1 +1,22 @@
-idadnjnaj
+                     <div class="input-groups">
+                       <form action="action.php" method="post" enctype="multipart/form-data">
+                       <input type="file" name="image" id="profile-img" required/><br>
+                                    <img src="" id="profile-img-tag" width="100px" />
+
+                                    <script type="text/javascript">
+                                        function readURL(input) {
+                                            if (input.files && input.files[0]) {
+                                                var reader = new FileReader();
+                                                
+                                                reader.onload = function (e) {
+                                                    $('#profile-img-tag').attr('src', e.target.result);
+                                                }
+                                                reader.readAsDataURL(input.files[0]);
+                                            }
+                                        }
+                                        $("#profile-img").change(function(){
+                                            readURL(this);
+                                        });
+                                    </script><br>
+                                    </form>
+                        </div>    
